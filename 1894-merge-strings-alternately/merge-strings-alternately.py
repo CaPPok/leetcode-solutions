7aeae64fa43c1411588c04a653fd5efeb5bb4bc7
Bành Phú Hội <hoi.banhphu2710@hcmut.edu.vn>
2026-06-26 11:14:41 +0700

    Time: 40 ms (84.76%) | Memory: 19.3 MB (17.16%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,16 +1,16 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        rtype = ""
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ans = ""
+        l1 = len(word1)
+        l2 = len(word2)
         i = 0
-        while(i < len(word1) or i < len(word2)):
-            if(i < len(word1)):
-                rtype = rtype + word1[i]
-            if(i < len(word2)):
-                rtype = rtype + word2[i]
-            i += 1
-        return rtype
+        j = 0
+        while (i < l1 or j < l2):
+            if (i < l1):
+                ans = ans + word1[i]
+                i += 1
+            if (j < l2):
+                ans = ans + word2[j]
+                j += 1
+
+        return ans 
