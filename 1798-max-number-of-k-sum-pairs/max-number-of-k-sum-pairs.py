9793e4d733d8e46388bbe97eac5176adb9bbaf0f
Bành Phú Hội <hoi.banhphu2710@hcmut.edu.vn>
2026-06-30 00:35:46 +0700

    Time: 459 ms (75.53%) | Memory: 32.5 MB (6.01%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,20 +1,16 @@
+from collections import defaultdict
+
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        left = 0
-        right = len(nums) - 1
+        seen = defaultdict(int)
         operations = 0
         
-        while left < right:
-            current_sum = nums[left] + nums[right]
-            
-            if current_sum == k:
+        for num in nums:
+            target = k - num
+            if seen[target] > 0:
                 operations += 1
-                left += 1
-                right -= 1
-            elif current_sum < k:
-                left += 1
+                seen[target] -= 1
             else:
-                right -= 1
+                seen[num] += 1
                 
         return operations
