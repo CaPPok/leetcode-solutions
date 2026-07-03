f1203f53c703136aa26aac5f8f78044faebdc5b7
Bành Phú Hội <hoi.banhphu2710@hcmut.edu.vn>
2026-07-04 01:04:52 +0700

    Time: 3 ms (6.10%) | Memory: 19.2 MB (85.11%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        highest = 0
+        current = 0
+
+        for i in gain:
+            current = current + i
+            if current > highest:
+                highest = current
+        return highest
+
+
